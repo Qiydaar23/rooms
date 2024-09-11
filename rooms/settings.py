2e623bc7d7c247571949933a13e114f392d13809
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-09-11 16:53:25 -0400

    done

--- rooms/settings.py
+++ rooms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*z^i&nn7^_3*yf=#qk$@&#*(y$%_i$%0j9$2^gab5^jq5$ee-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.143.225.56']
 
 
 # Application definition
